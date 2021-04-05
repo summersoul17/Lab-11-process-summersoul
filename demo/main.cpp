613aac33304e0764160670a743ea97d6ad829b69
mrmamongo <puper.e@yandex.ru>
2021-04-06 01:52:31 +0300

    remove pdata and watch timer error

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,9 @@
-#include <header.hpp>
+#include <builder.hpp>
+#include <iostream>
 
-int main() {
 
+int main(int argc, char** argv) {
+  builder b{};
+  log_setup::init();
+  return b.vmain(b.parse_console_args(argc, argv));
 }
